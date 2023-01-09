2649c0ce0434149dd9f405ceab318430e0e32645
Igor-Zhelezniak-1 <90275833+Igor-Zhelezniak-1@users.noreply.github.com>
2023-01-09 09:05:21 -0500

    -

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@
  *
  * By: Igor
  * Version: 1.0
- * Since:   2022-07-21
+ * Since:   2022-11-26
  */
 
 const MAGIC_NUM = 15
 const NUM_ARR = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 // Process
-function findDuplicates(arr: number[]): number[] {
+function duplicates(arr: number[]): number[] {
   const sortedArr = arr.slice().sort(function (a, b) {
     return a - b
   })
@@ -33,7 +33,7 @@ ${arr[3]} ${arr[4]} ${arr[5]}\n${arr[6]} ${arr[7]} ${arr[8]}`
 
 //Process
 function isMagic(square: number[]): boolean {
-  const hasDuplicates = findDuplicates(square).length === 0
+  const hasDuplicates = duplicates(square).length === 0
   const rows =
     square[0] + square[1] + square[2] === MAGIC_NUM &&
     square[3] + square[4] + square[5] === MAGIC_NUM &&
